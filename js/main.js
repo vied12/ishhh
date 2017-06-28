54ee611a9d35904fdb07e3db7be72be2ed0b7bf5
Edouard Richard <edou4rd@gmail.com>
2017-06-28 13:58:17 +0200

    parallax

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+function updatePosition() {
+  var backgrounds = $('.background')
+  var newPos = window.pageYOffset / 2;
+  translateY(backgrounds, newPos);
+}
 
+function translateY(backgrounds, value) {
+  var translate = 'translateY(' + value + 'px)';
+  backgrounds.each(function(elm) {
+      this.style['-webkit-transform'] = translate;
+      this.style['-moz-transform'] = translate;
+      this.style['-ms-transform'] = translate;
+      this.style['-o-transform'] = translate;
+      this.style.transform = translate;
+  })
+}
+
+window.addEventListener('scroll', updatePosition, false);
