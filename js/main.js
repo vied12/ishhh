47ad70051adee713beea60177ea9eec3173ee002
Edouard Richard <edou4rd@gmail.com>
2017-07-01 22:22:34 +0200

    more pics

--- js/main.js
+++ js/main.js
@@ -18,15 +18,34 @@ function translateY(backgrounds, value) {
 window.addEventListener('scroll', updatePosition, false);
 
 const images = [
-    'MG_3438.jpg',
-    'MG_3560.jpg',
-    'MG_3241.jpg',
+    {img: 'MG_3345.jpg', color: '#2c7373', logo: 'logo-Ishhh-vert-jaune.eps.png'},
+    {img: 'MG_3438.jpg', color: 'rgb(251, 247, 251)', logo: 'logo-Ishhh-bleu-vert.eps.png', text: 'black'},
+    {img: 'MG_3518.jpg', color: '#0032b8', logo: 'ishhh-dégradé-BD.png'},
+    {img: 'MG_3560.jpg', color: 'rgb(251, 247, 251)', logo: 'logo-Ishhh-jaune-rouge.eps.png', text: 'black'},
+    {img: 'MG_3241.jpg', color: '#e94c45', logo: 'ishhh.png'},
 ]
 var index = 0
+function isMobile() {
+    return $('.marge-background').css('position') !== 'fixed'
+}
 function changeImage() {
+    if (isMobile()) {
+        return
+    }
     var margeBkg = $('.marge-background')
+    var logo = $('.logo')
+    logo.fadeOut(1000, () => {
+        logo.attr('src', images[index].logo)
+        logo.fadeIn(5000)
+    })
+    $('body, a, a:hover, a:active, a:visited').css({
+        color: images[index].text || 'white',
+    })
     margeBkg.fadeOut(1000, () => {
-        margeBkg.css('backgroundImage', `url(../${images[index]})`).fadeIn(5000)
+        margeBkg.css('backgroundImage', `url(../${images[index].img})`).fadeIn(5000)
+        $('body').css({
+            backgroundColor: images[index].color,
+        })
         index = (index + 1) % images.length
     })
 }
