17e03a88d5d0cfc0f95f6780b7be6e720e35d213
Edouard Richard <vied12@gmail.com>
2018-05-28 22:54:25 +0200

    Header: scroll down

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import withStyle from 'react-jss'
 import CrossfadeImage from 'components/CrossfadeImage'
 import { pictures } from 'pictures'
 import logo from 'images/ishhh-NB-MD.png'
+import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown'
+import Button from '@material-ui/core/Button'
 
 const styles = ({
   root: {
@@ -27,7 +29,20 @@ const styles = ({
       objectPosition: '50% 50%',
     },
   },
-
+  goDown: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    margin: [0, 'auto'],
+    zIndex: 3,
+    color: 'white',
+    width: 50,
+    height: 50,
+    minHeight: 'unset',
+    minWidth: 'unset',
+    padding: 'unset',
+  }
 })
 
 class Header extends React.Component {
@@ -52,6 +67,14 @@ class Header extends React.Component {
     }
     this.nextImage()
     this.startInterval()
+  }
+
+  scrollDown = () => {
+    window.scroll({
+      top: window.innerHeight - 50,
+      left: 0,
+      behavior: 'smooth'
+    })
   }
 
   render() {
@@ -89,6 +112,11 @@ class Header extends React.Component {
             imgStyle={{ width: '100%', height: '100%' }}
           />
         </div>
+        <Button className={classes.goDown}
+          onClick={this.scrollDown}
+        >
+          <KeyboardArrowDown style={{ width: 50, height: 50 }}/>
+        </Button>
       </header>
     )
   }
