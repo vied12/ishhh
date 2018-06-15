3cd6f0df9a6399f0a327fb881354dee51f8eae3f
Edouard Richard <vied12@gmail.com>
2018-06-15 10:08:26 +0200

    picture updated

--- src/pictures/index.js
+++ src/pictures/index.js
@@ -1,13 +1,13 @@
 import IMG_4948 from 'pictures/2018/IMG_4948 copie.jpg'
 import IMG_4979 from 'pictures/2018/IMG_4979 copie.jpg'
-import IMG_4950 from 'pictures/2018/IMG_4950.JPG'
+import IMG_4967 from 'pictures/2018/IMG_4967 copie.jpg'
 import IMG_5012 from 'pictures/2018/IMG_5012 copie.jpg'
 import IMG_5023 from 'pictures/2018/IMG_5023 copie.jpg'
 import IMG_5026 from 'pictures/2018/IMG_5026 copie.jpg'
 import IMG_5046 from 'pictures/2018/IMG_5046 copie.jpg'
 import IMG_5048 from 'pictures/2018/IMG_5048 copie.jpg'
 import IMG_5078 from 'pictures/2018/IMG_5078 copie.jpg'
-import IMG_5136 from 'pictures/2018/IMG_5136 copie.jpg'
+import IMG_5142 from 'pictures/2018/IMG_5142 copie.jpg'
 import IMG_5144 from 'pictures/2018/IMG_5144 copie.jpg'
 import IMG_5147 from 'pictures/2018/IMG_5147 copie.jpg'
 import IMG_5169 from 'pictures/2018/IMG_5169 copie.jpg'
@@ -70,16 +70,16 @@ import MG_3518 from 'pictures/2017/MG_3518.jpg'
 import MG_3560 from 'pictures/2017/MG_3560.jpg'
 
 export const pictures = [
-  IMG_4979,
+  IMG_4967,
   IMG_4948,
-  IMG_4950,
+  IMG_4979,
   IMG_5012,
   IMG_5023,
   IMG_5026,
   IMG_5046,
   IMG_5048,
   IMG_5078,
-  IMG_5136,
+  IMG_5142,
   IMG_5144,
   IMG_5147,
   IMG_5175,
